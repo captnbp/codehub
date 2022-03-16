ca9a7687b0f96fd3c9ce66a6f569f8e3ba527f02
Benoit Pourre <benoit.pourre@gmail.com>
2022-03-16 09:21:35 +0100

    Bump chart

--- dockerfiles/code-server/docker-entrypoint.sh
+++ dockerfiles/code-server/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 touch /home/coder/.zshrc
 
 if [ "$1" = 'code-server' ]; then
-  exec /usr/bin/code-server --host 127.0.0.1 --port 8080 --auth none /home/coder
+  exec /usr/bin/code-server --bind-addr 0.0.0.0:8080 --auth none /home/coder
 fi
 
 exec "$@"
