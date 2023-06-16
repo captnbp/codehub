1ba78c161db0d47d7c8eff053f15ce7dc151e44b
Benoit Pourre <benoit.pourre@gmail.com>
2023-06-16 22:22:17 +0200

    Fix trivy
    Signed-off-by: Benoit Pourre <benoit.pourre@gmail.com>

--- dockerfiles/code-server/install_tools.sh
+++ dockerfiles/code-server/install_tools.sh
@@ -8,7 +8,7 @@ echo "Install tools"
 apt-get update >/dev/null
 apt-get install --no-install-recommends -y wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | apt-key add -
-echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | tee -a /etc/apt/sources.list.d/trivy.list
+echo deb https://aquasecurity.github.io/trivy-repo/deb buster main | tee -a /etc/apt/sources.list.d/trivy.list
 apt-get update >/dev/null
 apt-get dist-upgrade -y
 apt-get install --no-install-recommends -y vim pwgen jq unzip pass zsh fonts-powerline \
